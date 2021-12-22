e83753d18640bc4bd280da2e44e50ac9cd02a0e0
sadb0101 <sadb0101@naver.com>
2021-12-22 11:34:45 +0900

    app.js modify

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
 function App() {
+  useEffect(() => {
+    console.log('window.ReactNativeWebView');
+    console.log(window);
+    window.addEventListener("message", (event) => {
+      // alert("This is a Entry Point Working in iOS");
+      // console.log(event);
+      setTxt(JSON.stringify(event.data))
+      // init(event.data)
+   });
+  }, []);
+  const [txt, setTxt] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button
+          style={{width: 100, height: 100}}
+          onClick={() => {
+            setTxt(JSON.stringify(window.ReactNativeWebView));
+          }}
+        />
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+          onClick={() => {
+            window.ReactNativeWebView.postMessage('test');
+          }}
+        />
+        <button
+          style={{width: 100, height: 100}}
+          onClick={() => {
+            // document.ReactNativeWebView.postMessage('Post message from web');
+            window.ReactNativeWebView.postMessage('Post message from web');
+          }}
+        />
+        <p>{txt}</p>
+        <input type="file" />
       </header>
     </div>
   );
